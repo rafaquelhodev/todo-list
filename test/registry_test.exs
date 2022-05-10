e96eb4ec094aa94e30dc7b4526a6b3e72c22909d
Rafael <rafael.quelho@gmail.com>
2022-05-10 10:52:59 -0300

    adding manual test for registry

---
+++ test/registry_test.exs
@@ -0,0 +1,18 @@
+defmodule Registry.Test do
+
+  use ExUnit.Case, async: false
+
+  test "registry todo database worker" do
+    Todo.System.start_link()
+
+    [{old_worker_2_pid,_}] = Registry.lookup(Todo.Registry, {Todo.DatabaseWorker, 2})
+
+    ## killing worker 2
+    true = Process.exit(old_worker_2_pid, :kill)
+
+    [{new_worker_2_pid,_}] = Registry.lookup(Todo.Registry, {Todo.DatabaseWorker, 2})
+
+    assert old_worker_2_pid != new_worker_2_pid
+  end
+
+end
